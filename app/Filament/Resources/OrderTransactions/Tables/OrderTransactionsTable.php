40370d58b9104909f1c0652e309b594e23c0efe2
Maulana Rasoky <rasoky.maulana@gmail.com>
2026-09-13 11:10:09 +0900

    fix: show order ID in order transactions

--- app/Filament/Resources/OrderTransactions/Tables/OrderTransactionsTable.php
+++ app/Filament/Resources/OrderTransactions/Tables/OrderTransactionsTable.php
@@ -24,7 +24,7 @@ class OrderTransactionsTable
             ->columns([
                 //
                 TextColumn::make('name')->searchable(),
-                TextColumn::make('Order_trx_id')->searchable()->label('Order ID'),
+                TextColumn::make('order_trx_id')->searchable()->label('Order ID'),
                 TextColumn::make('created_at'),
                 IconColumn::make('is_paid')->boolean()->trueColor('success')->falseColor('danger')->trueIcon('heroicon-o-check-circle')->falseIcon('heroicon-o-x-circle')->label('Terverifikasi'),
             ])
